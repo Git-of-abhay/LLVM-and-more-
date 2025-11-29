09a6e2658356f9f92a8c435b04f8afd1ba7313d0
Abhay Sharma <abcodeforces@gmail.com>
2025-11-29 14:46:22 +0530

    Implement basic string output handling in C++
    ->understanding on abstract level what i want to do

---
+++ basic1.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define dxt string::npos
+int main(){
+    string line;
+    getline(cin,line);
+    size_t pos=line.find("output");
+    if(pos==dxt) cerr<<"error: expected 'output'\n";
+    else{
+        size_t pos1=line.find('"',pos+6);
+        if(pos1==dxt) cerr<<"Error: Expected opening after output '\"'\n";
+        else{
+            size_t pos2=line.find('"',pos1+1);
+            if(pos2==dxt) cerr<<"Error: Expected closing  after output '\"'\n";
+            else{
+                string toprint = line.substr(pos1+1,pos2-pos1 - 1 );
+                cout<<toprint<<endl;
+            }
+        }
+    }
+    return 0;
+}
